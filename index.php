fd6943a68e40bc550e8776f3827d3cf6c24dbaa5
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-06-19 11:34:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<style type="text/css">
-  <?php include("bg-color.php") ?>
-</style>
+<title>LodedFromrhoc</title>
 
 <h1>Hello-World!! Let's See the date</h1>
 
@@ -8,11 +6,6 @@
 <h1>uploaded from openshift</h1>
 <pre>
 <?php
-//Don't forget to sanitize the input
-$colour = $_GET["pink"];
-?>
-body {
-    background-color: #<?php echo pink ?>;
-}
+echo `date`
 
 </pre>
